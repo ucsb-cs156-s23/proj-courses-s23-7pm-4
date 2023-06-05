fb580066917ac41ec461e4bb690b1aae5927b861
Brenna <brennascholte@ucsb.edu>
2023-06-05 12:55:27 -0700

    bs - talked to TA, suggested that I just need to define string variables in this file

--- src/main/java/edu/ucsb/cs156/courses/documents/Update.java
+++ src/main/java/edu/ucsb/cs156/courses/documents/Update.java
@@ -1,6 +1,5 @@
 package edu.ucsb.cs156.courses.documents;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -17,21 +16,4 @@ public class Update {
     private String subject_area;
     private String quarter_yyyyq;
     private String last_update;
-
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-
-        Update newUpdate = new Update();
-
-        String newSubjectArea = (String) this.getSubject_area().clone();
-        newUpdate.setSubject_area(newSubjectArea);
-
-        String newQuarter = (String) this.getQuarter_yyyyq().clone();
-        newUpdate.setQuarter_yyyyq(newQuarter);
-
-        String newLastUpdate = (String) this.getLast_update().clone();
-        newUpdate.setLast_update(newLastUpdate);
-
-        return newUpdate;
-    }
 }
